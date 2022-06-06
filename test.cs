13e856c6ac40cd1bf37288836e3753fa2137f550
Tien Bui <tien.bui@nois.vn>
2022-06-06 10:50:48 +0700

    variable

--- test.cs
+++ test.cs
@@ -3,9 +3,12 @@ using System;
 namespace Variable {
     class Program {
         static void Main(string[] args) {
-            int a = 254;
-            int b = 343;
+            int a = 6;
+            int b = 2;
             Console.WriteLine("a + b = " + (a + b));
+            Console.WriteLine("a - b = " + (a - b));
+            Console.WriteLine("a * b = " + (a * b));
+            Console.WriteLine("a / b = " + (a / b));
         }
     }
 }
