bd8ca50bc495ffbdd0ef160a6535427e84dc54b5
Tien Bui <tien.bui@nois.vn>
2022-06-06 10:55:34 +0700

    variable string

--- test.cs
+++ test.cs
@@ -3,12 +3,8 @@ using System;
 namespace Variable {
     class Program {
         static void Main(string[] args) {
-            int a = 6;
-            int b = 2;
-            Console.WriteLine("a + b = " + (a + b));
-            Console.WriteLine("a - b = " + (a - b));
-            Console.WriteLine("a * b = " + (a * b));
-            Console.WriteLine("a / b = " + (a / b));
+            string name = "Quang Tien";
+            Console.WriteLine("Hello " + name);
         }
     }
 }
