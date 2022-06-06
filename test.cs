5b347472ea13aeaf643164da61402f45692179f2
Tien Bui <tien.bui@nois.vn>
2022-06-06 13:35:27 +0700

    input output

--- test.cs
+++ test.cs
@@ -1,10 +1,14 @@
 using System;
 
-namespace Variable {
-    class Program {
-        static void Main(string[] args) {
-            double a = 10.5, b = 7;
-            Console.WriteLine("a / b = " + (a/b));
+namespace Input
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int a = int.Parse(Console.ReadLine());
+            int b = int.Parse(Console.ReadLine());
+            Console.WriteLine(a > b);
         }
     }
 }
