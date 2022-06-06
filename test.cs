0c57f01c905a7f5943da65998ca6d9ffdc7ee203
Tien Bui <tien.bui@nois.vn>
2022-06-06 10:58:46 +0700

    variable float

--- test.cs
+++ test.cs
@@ -3,8 +3,8 @@ using System;
 namespace Variable {
     class Program {
         static void Main(string[] args) {
-            string name = "Quang Tien";
-            Console.WriteLine("Hello " + name);
+            double a = 10.5, b = 7;
+            Console.WriteLine("a / b = " + (a/b));
         }
     }
 }
